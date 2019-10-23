ac580df2eab0a0ab5e3806db78b5c962bbdff707
Nguyen.Van.Hoang2 <Nguyen.Van.Hoang2@usol-v.com.vn>
2019-10-23 15:38:41 +0700

    add search list

--- src/main/java/t1708e/asm/diduduadi/config/ServiceConfig.java
+++ src/main/java/t1708e/asm/diduduadi/config/ServiceConfig.java
@@ -1,7 +1,29 @@
 package t1708e.asm.diduduadi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import t1708e.asm.diduduadi.service.search.SearchService;
+import t1708e.asm.diduduadi.service.search.SearchServiceServiceLocator;
+import t1708e.asm.diduduadi.service.user.UserService;
+import t1708e.asm.diduduadi.service.user.UserServiceServiceLocator;
+
+
+import javax.xml.rpc.ServiceException;
 
 @Configuration
 public class ServiceConfig {
+
+    @Bean
+    UserService userService() throws ServiceException {
+        UserServiceServiceLocator locator = new UserServiceServiceLocator();
+        UserService userService = locator.getUserServicePort();
+        return userService;
+    }
+
+    @Bean
+    SearchService searchService() throws ServiceException {
+        SearchServiceServiceLocator locator = new SearchServiceServiceLocator();
+        SearchService searchService = locator.getSearchServicePort();
+        return searchService;
+    }
 }
