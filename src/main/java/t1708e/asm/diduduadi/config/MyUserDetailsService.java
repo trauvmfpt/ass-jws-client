3ffa3026303e82e1d690810a44d51e752afc0f92
JUstSaLtPiLe <vu.tienphuc97@gmail.com>
2019-10-24 23:18:01 +0700

    xong authentication bang spring security

---
+++ src/main/java/t1708e/asm/diduduadi/config/MyUserDetailsService.java
@@ -0,0 +1,35 @@
+package t1708e.asm.diduduadi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import t1708e.asm.diduduadi.entity.Role;
+import t1708e.asm.diduduadi.service.user.UserService;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        t1708e.asm.diduduadi.entity.User user = null;
+        try {
+            user = userService.getByUserName(s);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        UserDetails userDetails =
+                User.builder()
+                        .username(user.getUsername())
+                        .password(user.getPassword())
+                        .roles("guide")
+                        .build();
+        return userDetails;
+    }
+}
